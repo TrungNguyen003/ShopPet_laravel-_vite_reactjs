d19e2e7988a46b1663510c0c7afa7146b00c9d8e
linh2805 <bangtinhuyen@gmail.com>
2025-08-06 10:03:18 +0700

    prj

---
+++ resources/js/router/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>Welcome to Shop Pet, your go-to place for all pet supplies!</p>
+        </div>
+    );
+}
+
+export default About;
