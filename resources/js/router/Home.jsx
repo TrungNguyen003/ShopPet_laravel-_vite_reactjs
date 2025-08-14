3489964e2299c53137824dc8ca8d22ddc31c79c5
dungdeptraivc <nguyendoandung123456789@gmail.com>
2025-08-14 17:07:55 +0700

    14/8

--- resources/js/router/Home.jsx
+++ resources/js/router/Home.jsx
@@ -26,16 +26,16 @@ const Home = () => {
                             <h2> DoDu Pets</h2>
                         </div>
                         <div className="text-center ">
-                            <h6>Chúng tôi sở hữu các sản phẩm chất lượng cao trong danh mục sản phẩm của mình và
-                                là <br />công ty nội địa đầu tiên sử dụng thịt làm thực phẩm cho chó để sản xuất thức ăn thô.<br />
-                                Ngoài ra, chúng tôi còn sản xuất thức ăn chế biến sẵn và thức ăn nhai cho chó. <br />Chúng tôi
-                                cũng có thức ăn cho mèo chất  lượng cao trong danh mục sản phẩm của mình.</h6>
+                            <h5>Chúng tôi sở hữu các sản phẩm chất lượng cao trong danh mục sản phẩm <br /> của mình và
+                                là công ty nội địa đầu tiên sử dụng thịt làm thực phẩm cho chó để sản<br /> xuất thức ăn thô.
+                                Ngoài ra, chúng tôi còn sản xuất thức ăn chế biến sẵn và <br />thức ăn nhai cho chó. Chúng tôi
+                                cũng có thức ăn cho mèo chất  lượng cao trong<br /> danh mục sản phẩm của mình.</h5>
                         </div>
                     </div>
                 </div>
                 <div className="col-sm-12 col-md-4 col-lg-4">
-                    <div className="img-pet mt-4">
-                        <img src="/source/anhpet.png" alt="Banner" className="img-fluid" />
+                    <div className=" mt-4">
+                       <img src="/source/anhpet.png" alt="Banner" className="img-pet " />
                     </div>
                 </div>
             </div>
@@ -56,7 +56,7 @@ const Home = () => {
                     </div>
                     <div className="col-sm-12 col-md-6 col-lg-3">
                         <div className="">
-                            <img src="/source/dogcat.png" alt="Banner" className="img-teritory" />
+                            <img src="/source/catdog.png" alt="Banner" className="img-teritory" />
                         </div>
                     </div>
                     <div className="col-sm-12 col-md-6 col-lg-3">
@@ -65,8 +65,16 @@ const Home = () => {
                         </div>
                     </div>
                 </div>
-                <div className="container mt-5 mb-5">
-                    <img src="/source/comment.png" alt="Banner" className="" />
+                <div className=" row mt-4">
+                    <div className="col-sm-12 col-md-12 col-lg-4">
+                        <img src="/source/anh4.png" alt="Banner" className="img-teritory mt-5" />
+                    </div>
+                    <div className="col-sm-12 col-md-12 col-lg-4">
+                        <img src="/source/anh5.png" alt="Banner" className="img-teritory mt-2" />
+                    </div>
+                    <div className="col-sm-12 col-md-12 col-lg-4">
+                        <img src="/source/anh6.png" alt="Banner" className="img-teritory mt-5" />
+                    </div>
                 </div>
             </div>
             <div>
@@ -141,7 +149,21 @@ const Home = () => {
                 </div>
             </div>
             <div className="container mt-5 mb-3">
-                <img src="/source/anh2.png" alt="Banner" className="w-100" />
+                <div className="row bg-pet" >
+                    <div className="col-sm-12 col-md-12 col-lg-8">
+                        <div className="p-5">
+                            <h3>Chúng tôi luôn mang lại những sản phẩm tốt nhất cho <br /> thú cưng của bạn !</h3>
+                            <p>Chất lượng cao, an toàn: Sản phẩm tại PetHaven đều được kiểm  định chặt chẽ, an toàn và <br /> phù hợp cho sức khỏe thú cưng.</p>
+                            <p>Thức ăn ngon, dinh dưỡng: Thức ăn đa dạng, chuẩn vị tự nhiên,  hấp dẫn khẩu vị và đảm bảo <br /> dinh dưỡng cho thú cưng.</p>
+                            <p> Thân thiện với môi trường: Sản phẩm được thiết kế để bảo vệ môi  trường, từ bao bì <br /> đến các vật dụng vệ sinh.</p>
+                        </div>
+                    </div>
+                    <div className="col-sm-12 col-md-12 col-lg-4">
+                        <div className="p-5">
+                            <img src="/source/anh7.png" alt="" className="w-100" />
+                        </div>
+                    </div>
+                </div>
             </div>
             <div>
                 <div className="container">
@@ -266,10 +288,56 @@ const Home = () => {
                     </div>
                 </div>
             </div>
-            <div>
-                  <div className="text-center mt-3">
-                        <h2 className=" section-title">Tin tức & kinh nghiệm  </h2>
+            <div className="container mt-5">
+                <div className="text-center mt-3">
+                    <h2 className=" section-title">Tin tức & kinh nghiệm  </h2>
+                </div>
+                <div className="row mt-4 mb-5">
+                    <div className="col-sm-12 col-md-6 col-lg-3">
+                        <div className="text-news">
+                            <img src="/source/anh8.png" alt="" className="w-100 text-news" />
+                            <h5 className="p-3">Cách chăm sóc thú cưng của mình khi bị ốm, và cách sơ cứu,..</h5>
+                            <div className="text-center">
+                                <div className="button-detail mb-3  ">
+                                    <h5 className="mt-1">Xem chi tiết</h5>
+                                </div>
+                            </div>
+                        </div>
                     </div>
+                    <div className="col-sm-12 col-md-6 col-lg-3">
+                        <div className="text-news">
+                            <img src="/source/anh8.png" alt="" className="w-100 text-news" />
+                            <h5 className="p-3">Cách chăm sóc thú cưng của mình khi bị ốm, và cách sơ cứu,..</h5>
+                            <div className="text-center">
+                                <div className="button-detail mb-3  ">
+                                    <h5 className="mt-1">Xem chi tiết</h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-sm-12 col-md-6 col-lg-3">
+                        <div className="text-news">
+                            <img src="/source/anh8.png" alt="" className="w-100 text-news" />
+                            <h5 className="p-3">Cách chăm sóc thú cưng của mình khi bị ốm, và cách sơ cứu,..</h5>
+                            <div className="text-center">
+                                <div className="button-detail mb-3  ">
+                                    <h5 className="mt-1">Xem chi tiết</h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-sm-12 col-md-6 col-lg-3">
+                        <div className="text-news">
+                            <img src="/source/anh8.png" alt="" className="w-100 text-news" />
+                            <h5 className="p-3">Cách chăm sóc thú cưng của mình khi bị ốm, và cách sơ cứu,..</h5>
+                            <div className="text-center">
+                                <div className="button-detail mb-3  ">
+                                    <h5 className="mt-1">Xem chi tiết</h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     );
